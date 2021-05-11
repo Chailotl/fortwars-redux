f08494bd56dc043ac88fba42ac730cc09c1a7d27
Chailotl <7232280+Chailotl@users.noreply.github.com>
2021-05-11 07:58:46 -0400

    Basic breakable prop

---
+++ gamemodes/fortwars-redux/entities/entities/fw_prop.lua
@@ -0,0 +1,27 @@
+AddCSLuaFile()
+
+ENT.Type	= "anim"
+ENT.Name	= "Block"
+
+function ENT:SetupDataTables()
+	self:NetworkVar( "Int", 0, "Team" )
+	--self:NetworkVar( "Entity", 0, "Player" )
+end
+
+function ENT:Initialize()
+	if SERVER then
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetMaxHealth( 100 )
+		self:SetHealth( 100 )
+	end
+end
+
+function ENT:OnTakeDamage( dmginfo )
+	if dmginfo:IsDamageType( DMG_CLUB ) then
+		local health = self:Health() - dmginfo:GetDamage()
+		self:SetHealth( health )
+		if health <= 0 then
+			self:Remove()
+		end
+	end
+end

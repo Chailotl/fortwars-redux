b20d2914de994697785dd348a7b4955087f18b36
Chailotl <7232280+Chailotl@users.noreply.github.com>
2021-05-11 07:56:33 -0400

    Enabling the built-in team system

--- gamemodes/fortwars-redux/gamemode/shared.lua
+++ gamemodes/fortwars-redux/gamemode/shared.lua
@@ -2,7 +2,21 @@ GM.Name			= "Fortwars Redux"
 GM.Author		= "Chai"
 GM.Email		= "N/A"
 GM.Website		= "N/A"
+GM.TeamBased	= true
+
+TEAM_RED	= 1
+TEAM_BLUE	= 2
+
+ROUND_WAIT		= 1
+ROUND_BUILD		= 2
+ROUND_ACTIVE	= 3
+ROUND_POST		= 4
 
 function GM:Initialize()
 
+end
+
+function GM:CreateTeams()
+	team.SetUp( TEAM_RED, "Red Team", Color( 255, 0, 0 ) )
+	team.SetUp( TEAM_BLUE, "Blue Team", Color( 0, 0, 255 ) )
 end
